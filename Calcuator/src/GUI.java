e3e716d0ca93de4fb64a5012b059141049fb14d1
sachmathew <sachmathew@gmail.com>
2016-05-18 09:39:42 -0400

    GUI

--- Calcuator/src/GUI.java
+++ Calcuator/src/GUI.java
@@ -1,4 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
-public class GUI {
+public class GUI extends JFrame {
 
+	public GUI() {
+		createUserInterface();
+	}
+
+	private void createUserInterface() {
+		Container contentPane = getContentPane();
+		contentPane.setLayout(null);
+
+		setSize(500, 500);
+		setVisible(true);
+		setTitle("Calculator");
+
+	}
+	
+	public static void main(String[] args){
+		GUI calculator = new GUI();
+	}
 }
