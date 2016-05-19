4e5f04c5da7d70979030cb8008b1eec912787b6e
Blackhawk39 <paulhyu1234@gmail.com>
2016-05-19 19:30:37 -0400

    added some empty action listeners to the buttons for later use

--- Calcuator/src/GUI.java
+++ Calcuator/src/GUI.java
@@ -21,27 +21,97 @@ public class GUI extends JFrame {
 		add = new JButton("ADD");
 		add.setBounds(60, 300, 70, 30);
 		contentPane.add(add);
+		add.addActionListener(
+
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent event)
+					{
+					
+				
+					}		
+				}
+
+				);
 	
 		sub = new JButton("SUB");
 		sub.setBounds(200, 300, 70, 30);
 		contentPane.add(sub);
+		sub.addActionListener(
+
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent event)
+					{
+					
+				
+					}		
+				}
+
+				);
 		
 		mul = new JButton("MUL");
 		mul.setBounds(340, 300, 70, 30);
 		contentPane.add(mul);
+		mul.addActionListener(
+
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent event)
+					{
+					
+				
+					}		
+				}
+
+				);
 		
 		div = new JButton("DIV");
 		div.setBounds(60, 350, 70, 30);
 		contentPane.add(div);
+		div.addActionListener(
+
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent event)
+					{
+					
+				
+					}		
+				}
+
+				);
 	
 		mod = new JButton("MOD");
 		mod.setBounds(200, 350, 70, 30);
 		contentPane.add(mod);
-		
+		mod.addActionListener(
+
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent event)
+					{
+					
+				
+					}		
+				}
+
+				);
 		exp = new JButton("EXP");
 		exp.setBounds(340, 350, 70, 30);
 		contentPane.add(exp);
-		
+		exp.addActionListener(
+
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent event)
+					{
+					
+				
+					}		
+				}
+
+				);
 		setSize(500, 500);
 		setVisible(true);
 		setTitle("Calculator");
