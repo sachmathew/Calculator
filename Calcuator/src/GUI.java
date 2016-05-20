6f5360e52ea1f05b62a39152c03f3efb30baac4a
Blackhawk39 <paulhyu1234@gmail.com>
2016-05-20 11:31:23 -0400

    made textfields and labels

--- Calcuator/src/GUI.java
+++ Calcuator/src/GUI.java
@@ -1,22 +1,54 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.text.NumberFormat;
 
 import javax.swing.*;
 import javax.swing.border.*;
+import javax.swing.text.NumberFormatter;
 
 public class GUI extends JFrame {
 	
 	private JButton add, sub, mul, div, mod, exp;
-	private JTextField numberBox;
-
+	private JFormattedTextField num1,num2,answer;
+	private JLabel label1,label2, label3;
 	public GUI() {
 		createUserInterface();
 	}
 
+	
 	private void createUserInterface() {
 		Container contentPane = getContentPane();
 		contentPane.setLayout(null);
 
+		//NumberFormat longFormat = NumberFormat.getIntegerInstance();
+		//NumberFormatter numberFormatter = new NumberFormatter(longFormat);
+		//numberFormatter.setAllowsInvalid(false); 
+
+		
+		num1 = new JFormattedTextField();
+		num1.setBounds(100, 10, 300, 30);
+		contentPane.add(num1);
+		
+		num2 = new JFormattedTextField();
+		num2.setBounds(100, 50, 300, 30);
+		contentPane.add(num2);
+		
+		answer = new JFormattedTextField();
+		answer.setEditable(false);
+		answer.setBounds(100, 90, 300, 30);
+		contentPane.add(answer);
+		
+		label1 = new JLabel("#1:");
+		label1.setBounds(50, 10, 300, 30);
+		contentPane.add(label1);
+		
+		label2 = new JLabel("#2:");
+		label2.setBounds(50, 50, 300, 30);
+		contentPane.add(label2);
+		
+		label3 = new JLabel("Answer");
+		label3.setBounds(50, 90, 300, 30);
+		contentPane.add(label3);
 		
 		add = new JButton("ADD");
 		add.setBounds(60, 300, 70, 30);
